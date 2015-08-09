f1c5188ddd82a14761a342e542fc73bb14d5e66a
karynntran <karynn.tran@gmail.com>
2015-08-08 22:15:11 -0400

    Added some tests for spouse and marriage age
    Next step is to abstract code

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 var names = [1,2,3,4,5];
-var luckyNumber = 10;
-var eliminateThis = luckyNumber -1;
 
 var spouse = [];
+var marriageAge = [];
+var honeymoon = [];
+var numberOfKids = [];
 
 function grabValues(categoryID, array){
 	var numArray = [1,2,3,4,5];
@@ -14,22 +15,19 @@ function grabValues(categoryID, array){
 }
 
 var eliminationLoop = function(array, number){
-	
+	var eliminateThis = number -1;
 	if (array.length > 1) {
-		console.log(array);
 		if (array.length >= number){
 			var choiceAfter = array.splice(eliminateThis);
 			Array.prototype.unshift.apply(array,choiceAfter);
 			array.shift();
-			return eliminationLoop(array,luckyNumber);
+			return eliminationLoop(array,number);
 		} else {
 			var modNumber = number%array.length-1;
-			// console.log(array[modNumber]);
 			var choiceAfter = array.splice(modNumber);
 			Array.prototype.unshift.apply(array,choiceAfter);
 			array.shift();
-			// console.log(array);
-			return eliminationLoop(array,luckyNumber);
+			return eliminationLoop(array,number);
 		}
 	} else {
 		return array[0];
@@ -38,11 +36,19 @@ var eliminationLoop = function(array, number){
 
 
 document.addEventListener("DOMContentLoaded", function() {
-	document.getElementById('spouseButton').addEventListener('click', function(e){
+	document.getElementById('playMashButton').addEventListener('click', function(e){
 	    e.preventDefault();
 	    console.log("clicked");
+
+	    var luckyNumber = 5;
+
 	    grabValues("spouse", spouse);
-	    var answer = eliminationLoop(spouse,5);
-	    document.getElementById('spouseAnswer').innerHTML = answer;
+	    var spouseAnswer = eliminationLoop(spouse,5);
+	    document.getElementById('spouseAnswer').innerHTML = spouseAnswer;
+
+	    grabValues("marriageAge", marriageAge);
+	    var answer = eliminationLoop(marriageAge,5);
+	    document.getElementById('marriageAgeAnswer').innerHTML = answer;
+
 	})
 });
