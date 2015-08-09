d0a4d679d099991707e2c0d6d69a8aa30817801d
karynntran <karynn.tran@gmail.com>
2015-08-08 23:45:31 -0400

    Abstracted answer function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 // var names = [1,2,3,4,5];
+var diceArray = [1,2,3,4,5,6,7,8,9,10];
+var randomNumber;
 
 var spouse = [];
 var marriageAge = [];
@@ -16,7 +18,6 @@ function grabValues(categoryID, array){
 	for (var i=0; i < numArray.length; i++){
 		var option = document.getElementById("" + categoryID + numArray[i]).value;
 		array.push(option);
-		console.log(array);
 	};
 }
 
@@ -40,28 +41,49 @@ var eliminationLoop = function(array, number){
 	}
 }
 
+function getAnswers(luckyNumber){
+    var categories = [
+    	"spouse",
+    	"marriageAge",
+    	"honeymoon",
+    	"numberOfKids"
+    ];
+
+    for (var i=0; i < categories.length; i++){
+    	console.log(categories[i] + "");
+    	console.log(categories[i]);
+
+    	var string = categories[i] + "";
+    	var array = eval(categories[i]);
+
+	    grabValues(string, array);
+	    console.log(array);
+	    var answer = eliminationLoop(array,5);
+	    document.getElementById(string + 'Answer').innerHTML = answer;
+    }
 
 
+
+    // grabValues("marriageAge", marriageAge);
+    // var marriageAgeAnswer = eliminationLoop(marriageAge,5);
+    // document.getElementById('marriageAgeAnswer').innerHTML = marriageAgeAnswer;
+
+    // grabValues("honeymoon", honeymoon);
+    // var honeymoonAnswer = eliminationLoop(honeymoon,5);
+    // document.getElementById('honeymoonAnswer').innerHTML = honeymoonAnswer;
+
+    // grabValues("numberOfKids", numberOfKids);
+    // var numberOfKidsAnswer = eliminationLoop(numberOfKids,5);
+    // document.getElementById('numberOfKidsAnswer').innerHTML = numberOfKidsAnswer;
+}
 
 document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById('playMashButton').addEventListener('click', function(e){
 	    e.preventDefault();
-
-	    grabValues("spouse", spouse);
-	    var spouseAnswer = eliminationLoop(spouse,5);
-	    document.getElementById('spouseAnswer').innerHTML = spouseAnswer;
-
-	    grabValues("marriageAge", marriageAge);
-	    var marriageAgeAnswer = eliminationLoop(marriageAge,5);
-	    document.getElementById('marriageAgeAnswer').innerHTML = marriageAgeAnswer;
-
-	    grabValues("honeymoon", honeymoon);
-	    var honeymoonAnswer = eliminationLoop(honeymoon,5);
-	    document.getElementById('honeymoonAnswer').innerHTML = honeymoonAnswer;
-
-	    grabValues("numberOfKids", numberOfKids);
-	    var numberOfKidsAnswer = eliminationLoop(numberOfKids,5);
-	    document.getElementById('numberOfKidsAnswer').innerHTML = numberOfKidsAnswer;
-
+	    getAnswers(randomNumber);
+	})
+	document.getElementById('randomNumberButton').addEventListener('click', function(e){
+	    e.preventDefault();
+	    var randomNumber = diceArray[Math.floor(Math.random() * diceArray.length)];
 	})
 });
