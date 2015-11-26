20699e88a6d8a2b17e1599c8ac6118d042b931a4
karynntran <karynn.tran@gmail.com>
2015-11-25 23:24:30 -0500

    Complete gulpfile for automated tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,32 @@
 var gulp = require('gulp'),
-  connect = require('gulp-connect'),
   sass = require('gulp-ruby-sass'),
   autoprefixer = require('gulp-autoprefixer'),
   minifycss = require('gulp-minify-css'),
   rename = require('gulp-rename');
  
-gulp.task('webserver', function() {
-  connect.server();
+gulp.task('express', function() {
+  var express = require('express');
+  var app = express();
+  app.use(require('connect-livereload')({port: 35729}));
+  app.use(express.static(__dirname));
+  app.listen(4000, '0.0.0.0');
 });
+
+var tinylr;
+gulp.task('livereload', function() {
+  tinylr = require('tiny-lr')();
+    tinylr.listen(35729);
+});
+
+function notifyLiveReload(event) {
+  var fileName = require('path').relative(__dirname, event.path);
+
+  tinylr.changed({
+    body: {
+      files: [fileName]
+    }
+  });
+}
  
 gulp.task('styles', function() {
   return sass('sass', { style: 'expanded' })
@@ -19,8 +38,10 @@ gulp.task('styles', function() {
 
 gulp.task('watch', function() {
   gulp.watch('sass/*.scss', ['styles']);
+  gulp.watch('*.html', notifyLiveReload);
+  gulp.watch('css/*.css', notifyLiveReload);
 });
 
-gulp.task('default', ['webserver', 'watch'], function(){
+gulp.task('default', ['styles', 'express','livereload','watch'], function(){
 
 });
